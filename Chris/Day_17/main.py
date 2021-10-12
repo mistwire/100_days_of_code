bd0289169c41a51a54c00bc0d65ee39c98b94bca
Chris Williams <chrisfwilliams@gmail.com>
2021-10-12 16:53:13 -0400

    add day 17

---
+++ Chris/Day_17/main.py
@@ -0,0 +1,18 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for i in question_data:
+    question_bank.append(Question(i['question'], i['correct_answer']))
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number} ")
+
+

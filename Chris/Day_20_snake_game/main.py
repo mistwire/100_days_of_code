1047d5d96ff608ff0910cc21bd63246cec015f9c
Chris Williams <chrisfwilliams@gmail.com>
2021-10-20 21:02:13 -0400

    Day 20 finished!

--- Chris/Day_20_snake_game/main.py
+++ Chris/Day_20_snake_game/main.py
@@ -1,29 +1,27 @@
 import turtle
+import snake
+import time
 
 screen = turtle.Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("Snake Game!")
 screen.tracer(0)
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-segments = []
+my_snake = snake.Snake()
 
-for i in starting_positions:
-    new_segment = turtle.Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(i)
-    segments.append(new_segment)
+screen.listen()
+screen.onkey(my_snake.up, "Up")
+screen.onkey(my_snake.down, "Down")
+screen.onkey(my_snake.left, "Left")
+screen.onkey(my_snake.right, "Right")
+
 
 game_is_on = True
 while game_is_on:
-    for seg in segments:
-        seg.forward(20)
-
-
-
-
+    screen.update()
+    time.sleep(0.1)
+    my_snake.move()
 
 
 screen.exitonclick()
---
+++ Chris/Day_20_snake_game/turn_on_key.py
@@ -0,0 +1,21 @@
+import turtle
+
+screen = turtle.Screen()
+
+screen.listen()
+# screen.onkey("Up")
+# screen.onkey("Down")
+# screen.onkey("Left")
+# screen.onkey("Right")
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()

005cdc4dc811b8b388edd62b8b85c66cb5ee2d13
Chris Williams <chrisfwilliams@gmail.com>
2021-11-01 16:37:31 -0400

    Day 24 persistent high score finished

--- Chris/Day_24_files_directories_paths/food.py
+++ Chris/Day_24_files_directories_paths/food.py
@@ -9,7 +9,7 @@ class Food(Turtle):
         self.shape("circle")
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        self.color("blue")
+        self.color("red")
         self.speed("fastest")
         self.refresh()
 

72f600a06a004bda0e616f9f6dd0d2680bb7aea6
Chris Williams <chrisfwilliams@gmail.com>
2021-11-06 13:24:07 -0400

    Day 25 lessons 224-226

--- Chris/Day_24_files_directories_paths/notes.py
+++ Chris/Day_24_files_directories_paths/notes.py
@@ -1,6 +1,8 @@
 
 # How to access a file in python
-file = open("my_file.txt")
+file_path = 'C:/Users/chris/Desktop/my_file.txt'
+
+file = open(file_path)
 # read the file and save in contents variable:
 contents = file.read()
 print(contents)
@@ -8,17 +10,17 @@ print(contents)
 file.close()
 
 # This is the better way to open/close files. The with operator auto-closes after use:
-with open("my_file.txt") as file:
+with open(file_path) as file:
     contents = file.read()
     print(contents)
 
 # How to write to file:
-with open("my_file.txt", mode="w") as f:
+with open(file_path, mode="w") as f:
     f.write("My new text here")
 
 # How to append to a file:
-with open("my_file.txt", mode="a") as f:
+with open(file_path, mode="a") as f:
     f.write("\nAppended text here")
 
-with open("new_file.txt", mode="w") as f:
+with open(file_path, mode="w") as f:
     f.write("Text for newly created file")

ea858e19f564bd12be5d80cfa318b7d4d68e44bb
nathan.farrar <nathan.farrar@greenpages.com>
2021-11-18 08:46:18 -0500

    Day25

--- Nate/Day25/us-states-game-start/us-states-game-start/main.py
+++ Nate/Day25/us-states-game-start/us-states-game-start/main.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 image = "blank_states_img.gif"
 states_file = "50_states.csv"
+not_found_file = "not_found.csv"
 
 screen = turtle.Screen()
 screen.title("US States Game")
@@ -15,16 +16,31 @@ correct = 0
 game_on = True
 
 df = pd.read_csv(states_file)
+all_states = df.state.to_list()
+states_found = []
 
 while game_on:
     attempt = screen.textinput(title=f"{correct}/50 correct", prompt="Enter name of a US state")
-    state = df[df.state == attempt]
 
-    if len(state) > 0:
+    if attempt == "Exit":
+        break
+
+    if attempt in all_states:
+        state = df[df.state == attempt]
+        states_found.append(attempt)
         x_cord = int(state.x)
         y_cord = int(state.y)
         turtle.goto(x_cord, y_cord)
-        turtle.write(attempt, move=True)
+        turtle.write(state.state.item())
         correct += 1
 
-screen.exitonclick()
+states_not_found = all_states.copy()
+
+for i in states_found:
+    if i in states_not_found:
+        states_not_found.remove(i)
+
+df = pd.DataFrame(states_not_found, columns=["States Not Found"])
+df.to_csv(not_found_file, index=False)
+
+print(states_not_found)

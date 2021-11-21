6d36f1efe9bc9f8397a3e8b2a919c867328abdd8
Chris Williams <chrisfwilliams@gmail.com>
2021-11-21 14:39:27 -0600

    Day 27 *args and **kwargs notes

--- Chris/Day_27_Tkinter/notes.py
+++ Chris/Day_27_Tkinter/notes.py
@@ -1,29 +1,20 @@
 import tkinter
 
 window = tkinter.Tk()
+# to add title to window:
 window.title("My First GUI Program")
+# to change default size
 window.minsize(width=500, height=300)
 
-# Label
-my_label = tkinter.Label(text="Label Here")
-my_label.pack()
+# Label is a **kwargs entry:
+my_label = tkinter.Label(text="Label Here", font=("Arial", 24, "bold"))
+# need to specify how label gets displayed before it will show:
+my_label.pack(expand=True)  # <-- will center the label - https://docs.python.org/3/library/tkinter.html#the-packer
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# need to keep window on screen (has to be at the very end)
+# use mainloop to keep window on screen (has to be at the end)
 window.mainloop()
+
+
+def add(*args):
+    for i in args:
+        print(i)

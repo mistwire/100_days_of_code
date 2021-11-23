5e0101c0c845a0bedbee1f5be1e7c0e3cf15d9d5
Chris Williams <chrisfwilliams@gmail.com>
2021-11-23 13:33:27 -0600

    Day 27 tkinter notes

--- Chris/Day_27_Tkinter/notes.py
+++ Chris/Day_27_Tkinter/notes.py
@@ -9,7 +9,31 @@ window.minsize(width=500, height=300)
 # Label is a **kwargs entry:
 my_label = tkinter.Label(text="Label Here", font=("Arial", 24, "bold"))
 # need to specify how label gets displayed before it will show:
-my_label.pack(expand=True)  # <-- will center the label - https://docs.python.org/3/library/tkinter.html#the-packer
+my_label.pack()  # <-- will center the label - https://docs.python.org/3/library/tkinter.html#the-packer
+my_label["text"] = "New Text"  # Can change like a dict entry
+my_label.config(text="New Text using config")  # Or via config entry
+
+# Button
+
+def button_clicked():
+    print("I got clicked")
+
+def button_clicked_2():
+    my_label["text"] = line_input.get()
+
+
+button = tkinter.Button(text="Click Me", command=button_clicked_2)
+button.pack()
+
+# Entry (basically an input) - https://tcl.tk/man/tcl8.6/TkCmd/entry.htm
+line_input = tkinter.Entry(width=10)
+line_input.pack()
+
+
+# Challenge - make button click change label text
+
+# Challenge - make the Entry string show up as the label when you click the button
+
 
 # use mainloop to keep window on screen (has to be at the end)
 window.mainloop()

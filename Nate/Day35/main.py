31eb9f27c9d5580397c6cf4b6d1b2186caffabe3
nathan.farrar <nathan.farrar@greenpages.com>
2021-12-09 18:01:53 -0500

    Day35

--- Nate/Day35/main.py
+++ Nate/Day35/main.py
@@ -1,23 +1,45 @@
 import requests
-import datetime as dt
+from twilio.rest import Client
+import os
 
+TWILIO_ACCOUNT_SID = "AC9f71e683a403031c1c5a6dc4ec10959a"
+TWILIO_AUTH_TOKEN = "f3f4b7612f841e814fe051ba634e6c57"
 MY_LAT = 47
 MY_LONG = -53
-key = "f83bda1d52d74dba4cfb9c0e922b89f1"
+open_weather_key = "f83bda1d52d74dba4cfb9c0e922b89f1"
 url = "https://api.openweathermap.org/data/2.5/onecall"
 city_name = "Amesbury"
+MY_TWILIO_NUMBER = "+15304831979"
+MY_NUMBER = "+13039173568"
 
-params = {
-    "lat": MY_LAT,
-    "lon": MY_LONG,
-    "appid": key,
-    "exclude": "current,minutely,daily"
-}
 
-response = requests.get(url=url, params=params)
-response.raise_for_status()
+def send_twilio_msg(message):
+    account_sid = os.environ[TWILIO_ACCOUNT_SID]
+    auth_token = os.environ[TWILIO_AUTH_TOKEN]
+    client = Client(account_sid, auth_token)
 
-data = response.json()
+    message = client.messages \
+                    .create(
+                         body=message,
+                         from_=MY_TWILIO_NUMBER,
+                         to=MY_NUMBER
+                     )
+
+    print(message.sid)
+
+
+def get_weather_data():
+    params = {
+        "lat": MY_LAT,
+        "lon": MY_LONG,
+        "appid": open_weather_key,
+        "exclude": "current,minutely,daily"
+    }
+
+    response = requests.get(url=url, params=params)
+    response.raise_for_status()
+
+    return response.json()
 
 # for index, hour in enumerate(data['hourly']):
 #     if index < 12:
@@ -26,9 +48,6 @@ data = response.json()
 #                 print("Bring an umbrella")
 #             elif weather['id'] < 700:
 #                 print("It's going to snow")
-
-hourly_data = data['hourly'][:12]
-
 # for hour in hourly_data:
 #     for weather in hour['weather']:
 #         if weather['id'] < 600:
@@ -36,6 +55,9 @@ hourly_data = data['hourly'][:12]
 #         elif weather['id'] < 700:
 #             print("It's going to snow")
 
+
+data = get_weather_data()
+hourly_data = data['hourly'][:12]
 will_rain = False
 
 for hour in hourly_data:
@@ -44,5 +66,7 @@ for hour in hourly_data:
             will_rain = True
 
 if will_rain:
-    print("Bring an umbrella.")
+    send_twilio_msg("Bring an umbrella.")
+else:
+    print("It's not going to rain.")
 

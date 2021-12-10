bde329e4234f0fa1fac76f6bfc91b4576d913f3f
nathan.farrar <nathan.farrar@greenpages.com>
2021-12-10 09:29:26 -0500

    Day36

--- Nate/Day36/main.py
+++ Nate/Day36/main.py
@@ -20,11 +20,11 @@ newsapi_key = "624823829e3346a788845a6d849920a6"
 news_uri = "https://newsapi.org/v2/everything"
 
 
-def get_news():
+def get_news(query):
     import requests
 
     params = {
-            'q': stock_sym,
+            'q': query,
             'from': day_before,
             'to': yesterday,
             'apiKey': newsapi_key
@@ -48,26 +48,6 @@ def send_twilio_msg(message):
 
     print(message.sid)
 
-## STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
-
-
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
 
 yesterday = str(date.today() - dt.timedelta(days=1))
 day_before = str(date.today() - dt.timedelta(days=2))
@@ -92,13 +72,25 @@ percent_change = round(((delta_price / day_before_close) * 100), 2)
 
 change = abs(percent_change)
 
-if change > 5:
-    news = get_news()
-    print(news)
+if change > 2:
+    news = get_news("Tesla")
+    articles = news['articles'][:3]
+    message = ""
     if percent_change < 0:
+        message += f"TSLA: 🔻{percent_change}%\n"
         print(f"{stock_sym} went down by {change}%")
     elif percent_change > 0:
+        message += f"TSLA: 🔺{percent_change}%\n"
         print(f"{stock_sym} went up by {change}%")
     else:
         print(f"{stock_sym} had no change")
+
+    for article in articles:
+        headline = article['title']
+        brief = article['description']
+        message += f"Headline: {headline}\n"
+        message += f"Brief: {brief}\n\n"
+
+    send_twilio_msg(message)
+
 

c716a333ef351f1a8a47dc4fee454afbf17f3b1f
Chris Williams <chrisfwilliams@gmail.com>
2021-12-24 10:33:38 -0500

    Day 28 start

---
+++ Chris/Day_28_pomodoro-start/main.py
@@ -0,0 +1,39 @@
+import tkinter
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+gui = tkinter.Tk()
+gui.title("Pomodoro Timer")
+gui.config(padx=100, pady=50)
+gui.config(bg=YELLOW)
+
+canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_image = tkinter.PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_image)
+canvas.create_text(100, 135, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
+
+
+
+
+
+
+
+
+
+gui.mainloop()

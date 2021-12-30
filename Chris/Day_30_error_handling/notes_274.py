4a46a5961166169ec94148169309621b575f3cfb
Chris Williams <chrisfwilliams@gmail.com>
2021-12-30 15:05:12 -0500

    Day 30 start

---
+++ Chris/Day_30_error_handling/notes_274.py
@@ -0,0 +1,36 @@
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+# TODO 1. Create a dictionary in this format:
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+# while True:
+#     word = input("Enter a word: ").upper()
+#     try:
+#         output_list = [phonetic_dict[letter] for letter in word]
+#     except KeyError:
+#         print("Sorry, only letters in the alphabet please")
+#     else:
+#         print(output_list)
+#         break
+
+# This is more elegant:
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()

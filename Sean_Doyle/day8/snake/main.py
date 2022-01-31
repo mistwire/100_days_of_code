69fcba78fc98163604fc3d5be535a9a634b83285
Sean <r3tr0pi3@gmail.com>
2022-01-31 13:50:10 -0800

    Day 8, Snake Game complete!

---
+++ Sean_Doyle/day8/snake/main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+from turtle import Screen
+import time
+from snake import Snake
+from snake import Food
+from snake import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        print("nom nom nom")
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    if (
+        snake.head.xcor() > 280
+        or snake.head.xcor() < -280
+        or snake.head.ycor() > 280
+        or snake.head.ycor() < -280
+    ):
+        game_is_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+screen.exitonclick()
---
+++ Sean_Doyle/day8/snake/snake/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.1.1"
+from snake.snake import Snake
+from snake.food import Food
+from snake.scoreboard import Scoreboard

32a0ac7c65c673121a17027b4ca853b04ec6966a
John Harbin <jharbin@gmail.com>
2022-02-04 21:31:27 -0700

    Harbin's Updates - 2022-02-04
    Initial commit

---
+++ John/Day18-part1/main.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+
+
+timmy_the_turtle = Turtle()
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
---
+++ John/QuizGame/question_model.py
@@ -0,0 +1,9 @@
+class Question:
+
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+
+#new_q = Question("asdfasdf", "False")
+#print(new_q.text)

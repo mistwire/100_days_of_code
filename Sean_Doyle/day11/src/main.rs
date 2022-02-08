824a37031c85411bd65c8526cee7eded09a3d79f
Sean <r3tr0pi3@gmail.com>
2022-02-07 16:28:56 -0800

    Updated day 10 python, treehouse Rust

---
+++ Sean_Doyle/day11/src/main.rs
@@ -0,0 +1,84 @@
+#![warn(clippy::all, clippy::pedantic)]
+use std::io::stdin;
+
+#[derive(Debug)]
+struct Visitor {
+    name: String,
+    action: VisitorAction,
+    age: i8,
+}
+
+#[derive(Debug)]
+enum VisitorAction {
+    Accept,
+    AcceptWithNote { note: String },
+    Refuse,
+    Probation,
+}
+
+fn what_is_your_name() -> String {
+    let mut your_name = String::new();
+    stdin()
+        .read_line(&mut your_name)
+        .expect("Failed to read line");
+    your_name.trim().to_lowercase()
+}
+
+impl Visitor {
+    fn new(name: &str, action: VisitorAction, age: i8) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            action,
+            age,
+        }
+    }
+    fn greet_visitor(&self) {
+        match &self.action {
+            VisitorAction::Accept => println!("Welcome to the tree house. {}", self.name),
+            VisitorAction::AcceptWithNote { note } => {
+                println!("Welcome to the treehouse, {}", self.name);
+                println!("{}", note);
+                if self.age < 21 {
+                    println!("Do not serve alcohol to {}", self.name);
+                }
+            }
+            VisitorAction::Probation => println!("{} is now a probationary member", self.name),
+            VisitorAction::Refuse => println!("Do not allow {} in!", self.name),
+        }
+    }
+}
+
+fn main() {
+    let mut visitor_list = vec![
+        Visitor::new("Sally", VisitorAction::Accept, 45),
+        Visitor::new(
+            "Sean",
+            VisitorAction::AcceptWithNote {
+                note: String::from("Like's Beer"),
+            },
+            30,
+        ),
+        Visitor::new("Bill", VisitorAction::Refuse, 30),
+    ];
+
+    loop {
+        println!("Hello, what is your name? Simply leave name blank to EXIT");
+        let name = what_is_your_name();
+
+        let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
+
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => {
+                if name.is_empty() {
+                    break;
+                } else {
+                    println!("{} is not on the visitor list.", name);
+                    visitor_list.push(Visitor::new(&name, VisitorAction::Probation, 0));
+                }
+            }
+        }
+    }
+    println!("The final list of visitors:");
+    println!("{:?}", visitor_list);
+}

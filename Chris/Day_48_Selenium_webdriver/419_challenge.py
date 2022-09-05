e9bb02bb605bf47c27975ee82733a94d53abb152
Chris Williams <chrisfwilliams@gmail.com>
2022-09-05 13:24:57 -0400

    Day 48 finished

--- Chris/Day_48_Selenium_webdriver/419_challenge.py
+++ Chris/Day_48_Selenium_webdriver/419_challenge.py
@@ -10,6 +10,34 @@ driver = webdriver.Chrome(service=Service(executable_path=ChromeDriverManager().
 driver.get("https://orteil.dashnet.org/experiments/cookie/")
 cookie = driver.find_element(by=By.ID, value="cookie")
 
-while True:
-    cookie.click()
+money_available = driver.find_element(by=By.ID, value="money")
+print(money_available.text)
 
+buy_cursor = driver.find_element(by=By.ID, value="buyCursor")
+buy_grandma = driver.find_element(by=By.ID, value="buyGrandma")
+buy_factory = driver.find_element(by=By.ID, value="buyFactory")
+
+i = 0
+
+while i <= 601:
+    cookie.click()
+    try:
+        if int(money_available.text) >= 500:
+            buy_factory.click()
+        elif int(money_available.text) >= 100:
+            buy_grandma.click()
+        elif int(money_available.text) >= 50:
+            buy_cursor.click()
+        else:
+            continue
+    except:
+        None
+    # elif int(money_available.text) >= 100:
+    #     buy_grandma.click()
+    # else:
+    #     buy_cursor.click()
+    i += 1
+
+print(money_available.text)
+
+input("hit enter to close: ")

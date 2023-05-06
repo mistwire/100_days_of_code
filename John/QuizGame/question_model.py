381b782abac05f0f9ab133710196cea6f5124af4
Chris Williams <chrisfwilliams@gmail.com>
2023-05-06 15:15:05 -0400

    carve out sean doyle branch

--- John/QuizGame/question_model.py
+++
@@ -1,9 +0,0 @@
-class Question:
-
-    def __init__(self, text, answer):
-        self.text = text
-        self.answer = answer
-
-
-#new_q = Question("asdfasdf", "False")
-#print(new_q.text)
--- Nate/Day27/playground.py
+++
@@ -1,9 +0,0 @@
-
-def add(*nums):
-    result = 0
-    for n in nums:
-        result += n
-    return result
-
-
-print(add(1,2,3,4))

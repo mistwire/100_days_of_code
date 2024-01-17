d1f8bb6eb653366d69b06ce6e20adf15f40cadc7
Chris Williams <chrisfwilliams@gmail.com>
2024-01-16 19:20:40 -0500

    more chapter 10 updates

--- Chris/Chapter_10_Files/division_calculator.py
+++ Chris/Chapter_10_Files/division_calculator.py
@@ -1,6 +1,19 @@
-try:
-    print(5/0)
-except ZeroDivisionError:
-    print("You can't devide by zero!")
+print("Give me 2 numbers & I'll devide them.")
+print("Enter 'q' to quit")
 
-    
+while True:
+    first_number = input("\nFirst Number: ")
+    if first_number == 'q':
+        break
+    second_number = input("\nSecond Number: ")
+    if second_number == 'q':
+        break
+    try:
+        answer = int(first_number) / in(second_number)
+    except ZeroDivisionError:
+        print("You can't devide by 0!")
+    else:
+        print(answer)
+
+
+

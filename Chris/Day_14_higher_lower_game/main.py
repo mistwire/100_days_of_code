fa29ff739c8a7e186c5e429b8dfc79a605decf16
Chris Williams <chrisfwilliams@gmail.com>
2024-01-23 21:53:35 -0500

    Finish day 14

--- Chris/Day_14_higher_lower_game/main.py
+++ Chris/Day_14_higher_lower_game/main.py
@@ -4,30 +4,33 @@ import random
 import os
 
 score = 0
+game_continues = True
 
 def get_random_data():
     contestant = random.choice(data)
     return contestant
 
-def compare():
-    first_option = get_random_data()
+first_option = get_random_data()
+
+while game_continues:
+    os.system('clear') 
+    print(art.logo)
+    print(f"Your current score is: {score}")
     print(f"Compare A: {first_option['name']}, {first_option['description']}, from {first_option['country']}.")
+    print(f"cheat: {first_option['follower_count']}")
     print(art.vs)
     second_option = get_random_data()
     print(f"Against B: {second_option['name']}, {second_option['description']}, from {second_option['country']}.")
+    print(f"cheat: {second_option['follower_count']}")
     players_guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-    if players_guess +
-
-
-# TODO: print logo
-os.system('clear') 
-print(art.logo)
-
-
-
-# TODO: compare number of followers to see who 'wins'
-
-
-
-# TODO: increment the score or player loses and display final score
+    if first_option['follower_count'] > second_option['follower_count']:
+        answer = 'a'
+    else:
+        answer = 'b'
+    if players_guess == answer:
+        score += 1
+        first_option = second_option
+    else:
+        game_continues = False
+        print(f"Game over, your final score is: {score}")
 

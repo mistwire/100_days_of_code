22df378ca9bad632b006eec0909d350bf78f34b0
Chris Williams <chrisfwilliams@gmail.com>
2024-02-07 20:39:35 -0500

    restart day 16

--- Chris/Day_16_start/main.py
+++ Chris/Day_16_start/main.py
@@ -1,27 +1,4 @@
-# import another_module
-# print(another_module.another_variable)
-# from turtle import *
-# terry = Turtle()
-# print(terry)
-# terry.shape("turtle")
-# terry.color("CornflowerBlue")
-# terry.forward(100)
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
+from turtle import Turtle 
 
-from prettytable import PrettyTable
-table = PrettyTable()
-print(table)
-
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-
-print(table)
-
-table.add_column("Type", ["Electric", "Water", "Fire"])
-
-print(table)
-
-table.align = "l"
-
-print(table)
+timmy = Turtle()
+print(timmy)

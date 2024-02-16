d51c3afad55a8ecd4d259b389ba92d8edbc0738d
Chris Williams <chrisfwilliams@gmail.com>
2024-02-16 10:24:46 -0500

    finish day 17

---
+++ Chris/Day_17/quiz-game-start/main.py
@@ -0,0 +1,23 @@
+# https://opentdb.com/
+
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+
+question_bank = []
+
+for question in question_data:
+    new_question = Question(question['text'], question['answer'])
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+
+print("You've completed the quiz")
+print(f"Your final score was {quiz.score}/{quiz.question_number}")
+

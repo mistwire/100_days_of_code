ed7d0252669aeec7657b4794299d47258080b404
Chris Williams <17027446+mistwire@users.noreply.github.com>
2024-05-20 22:18:48 -0400

    start day 22

--- Chris/Day_22_pong/main.py
+++ Chris/Day_22_pong/main.py
@@ -1,55 +1,38 @@
-import turtle
-import paddle
-import time
-import ball
-import scoreboard
+from turtle import Turtle, Screen
 
-screen = turtle.Screen()
+screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
-screen.title("The Machine That Goes 'Pong'")
+screen.title("Pong")
 screen.tracer(0)
 
-r_paddle = paddle.Paddle((350, 0))
-l_paddle = paddle.Paddle((-350, 0))
-ball = ball.Ball()
-scoreboard = scoreboard.Scoreboard()
+paddle = Turtle()
+paddle.hideturtle()
+paddle.speed("fastest")
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+paddle.showturtle()
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
 
 screen.listen()
-screen.onkey(r_paddle.go_up, "Up")
-screen.onkey(r_paddle.go_down, "Down")
-screen.onkey(l_paddle.go_up, "w")
-screen.onkey(l_paddle.go_down, "s")
-
-ball.move_ball()
-
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(ball.move_speed)
-    ball.move_ball()
 
-    # Detect collision with ceiling / floor:
-    if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce_y()
-
-    # Detect collision with paddles
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() > -320:
-        ball.bounce_x()
-
-    # Detect out of bounds collision
-    if ball.xcor() > 385:
-        ball.reset_position()
-        scoreboard.l_point()
-
-    if ball.xcor() < -385:
-        ball.reset_position()
-        scoreboard.r_point()
 
 
 
 screen.exitonclick()
-
-
-
